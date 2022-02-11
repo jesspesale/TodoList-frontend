7c5cb47fdfb31a180591173891b82533b885043e
jesspesale <jess_pesale@aol.com>
2022-02-11 18:50:35 -0500

    tested ant design with adding a button to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from react </h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
 
 export default App;
+
