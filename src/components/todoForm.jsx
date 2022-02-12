d6ed91f1d73ed148e5e19de9375807afc34a76cb
jesspesale <jess_pesale@aol.com>
2022-02-12 08:33:58 -0500

    added Button and plusCircleFilled to TodoForm

--- src/components/todoForm.jsx
+++ src/components/todoForm.jsx
@@ -24,9 +24,12 @@ const TodoForm = ({onFormSubmit}) => {
             className="todo-form" >
          <Row gutter={20}>
             <Col xs={24} sm={24} md={17} lg={19} xl={20}>
-            
+                <Button></Button>
             </Col>
             <Col xs={24} sm={24} md={7} lg={5} xl={4}>
+                <Button type="primary" htmlType="submit" block>
+                    <PlusCircleFilled/>
+                </Button>
             </Col>
          </Row>   
         </Form>
