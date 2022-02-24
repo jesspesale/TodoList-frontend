b10fee46866cd10a834c82144dc5436466ed3a0c
jesspesale <jess_pesale@aol.com>
2022-02-23 19:44:20 -0500

    added load, get and create todo methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
 import { Button } from 'antd';
 // import TodoForm from "./components/TodoForm"
-import { loadTodos } from './actions/loadTodos';
+// import {loadTodos} from "./actions/todoService"
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Hello from react </h1>
-      <Button />
       <Button type="primary">Button</Button>
-      {/* {loadTodos()} */}
       {/* <TodoForm></TodoForm> */}
     </div>
   );
--- src/actions/todoService.js
+++ src/actions/todoService.js
@@ -2,12 +2,36 @@
 // const baseUrl = "localhost:3000/todos"; ??
 
 // const baseURL = `${process.env.REACT_APP_API_URL}/todos`;
-
 // export const loadTodos = () => {
-//     console.log("hi")
-//     return <h1>Hey</h1>
-//     // return fetch(baseUrl).then((res) => res.json()).then(data => console.log(data))
+//   // console.log("Hi")
+//   return fetch(baseUrl)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// };
 
-// }
+const baseUrl = "http://localhost:3000/todos";
 
+export const loadTodos = () => {
+  // console.log("Hi")
+  return fetch(baseUrl).then((res) => res.json()).then((data) => console.log(data))
+};
+
+
+export const getTodo = () => {
+    return fetch(`${baseUrl}\${id}`).then((res) => res.json())
+}
+
+
+export const createTodo = (todo) => {
+    return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: todo.title,
+            completed: todo.completed
+        }),
+    }).then((res) => res.json())
+};
 
