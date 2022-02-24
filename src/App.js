cbdaae1fb29f998f81a630519875b1e3d5b0354b
jesspesale <jess_pesale@aol.com>
2022-02-23 19:12:07 -0500

    backtracked and going over all react code from the start again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Button } from 'antd';
-import TodoForm from "./components/TodoForm"
+// import TodoForm from "./components/TodoForm"
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from react </h1>
+      <Button />
       <Button type="primary">Button</Button>
-      <TodoForm></TodoForm>
+      {/* <TodoForm></TodoForm> */}
     </div>
   );
 }
